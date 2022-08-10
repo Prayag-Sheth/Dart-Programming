ddba3de405dad278aab90b8ecffccbd3d34f3186
Prayag Sheth <71243716+Prayag-Sheth@users.noreply.github.com>
2022-08-10 09:31:10 +0530

    Add files via upload

---
+++ Functions N class/3. this_keyword.dart
@@ -0,0 +1,12 @@
+void main() {
+  Class_name class_object = new Class_name('Anything');
+}
+
+class Class_name {
+  String variable = 'k';
+
+  Class_name(String variable) {
+    this.variable = variable;
+    print(variable);
+  }
+}
